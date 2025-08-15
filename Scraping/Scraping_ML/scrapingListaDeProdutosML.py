4b7bf7dd9e74d0de2da56ac7de6a65890429ae62
Matheus de Andrade <mateusandradegermano@gmail.com>
2025-08-15 08:48:12 -0300

    Exportar para planilha implementado
    Agora ao final do código ele exporta os dados para uma planilha com as colunas "nome", "preço" e "valor".
    
    Novas funções serão implementadas!

--- Scraping/Scraping_ML/scrapingListaDeProdutosML.py
+++ Scraping/Scraping_ML/scrapingListaDeProdutosML.py
@@ -10,10 +10,7 @@ url = f"https://lista.mercadolivre.com.br/{produto}"
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 }
-
 response = requests.get(url, headers=headers)
-
-nome_arquivo = "ListaDeProdutos.xlsx"
 
 if response.status_code == 200:
 
@@ -32,7 +29,9 @@ if response.status_code == 200:
         imgTag = item.find("img")
         tituloItem = imgTag["title"] if imgTag and imgTag.has_attr("title") else "N/A"
 
-        preco = item.find("span", class_="andes-money-amount__fraction").text
+        divPreco = item.find("div", class_="poly-price__current")
+
+        preco = divPreco.find("span", class_="andes-money-amount__fraction").text
 
         link = item.find("a", class_="poly-component__title")["href"]
 
@@ -48,11 +47,12 @@ if response.status_code == 200:
 
     print(f"Encontramos {len(listaDeProdutos)} itens nesta página!")
 
-    if itemPesquisado:
-        print("Pesquisa concluida, items salvos!")
-    else:
-        print("Algum erro foi encontrado, tente novamente")
+    df = pd.DataFrame(dadosDosProdutos)
+    nomeArquivo = f"produtos_{produto}.xlsx"
+    df.to_excel(nomeArquivo, index=False)
+    
+    print(f"\nPlanilha '{nomeArquivo}' criada com sucesso!")
+    print(df)
 
-    print("Planilha salva com sucesso")
 else:
     print("Big foda")
